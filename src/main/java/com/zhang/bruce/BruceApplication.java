76a60230cf087472d6a32458194240ab236d156d
unknown <zhangpan@dangdang.com>
2021-11-02 16:12:23 +0800

    Initial commit

---
+++ src/main/java/com/zhang/bruce/BruceApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.bruce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BruceApplication.class, args);
+    }
+
+}
