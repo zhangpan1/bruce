2f7549cc609e3839c267afd80fe700765f4a75e9
zhangpan1 <852857754@qq.com>
2021-11-26 18:42:41 +0800

    保持面试节奏，恶劣的情况也要保持镇定

---
+++ src/main/java/com/zhang/bruce/algorithm/linked/ReseverLink.java
@@ -0,0 +1,34 @@
+package com.zhang.bruce.algorithm.linked;
+
+/**
+ * @author bruce
+ * @version 1.0
+ * @date 2021/11/26 18:34
+ */
+public class ReseverLink {
+    public ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val,ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(){
+
+    }
+}
