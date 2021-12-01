0d3e95f3c8f5cf12b827ef770b4ebaeec9c4dc8c
zhangpan1 <852857754@qq.com>
2021-12-01 19:10:28 +0800

    合并有序链表

---
+++ src/main/java/com/zhang/bruce/algorithm/linked/MergeLink.java
@@ -0,0 +1,52 @@
+package com.zhang.bruce.algorithm.linked;
+
+/**
+ * @author bruce
+ * @version 1.0
+ * @date 2021/12/1 19:04
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ *
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ */
+class MergeLink {
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
+    public ListNode mergeTwoLists1(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
+
+}
+
