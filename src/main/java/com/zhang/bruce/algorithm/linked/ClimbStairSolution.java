5273eeaff35938043bb5b4a5a48cc8eb1fbea4c6
zhangpan1 <852857754@qq.com>
2021-12-02 18:27:28 +0800

    删除链表倒数第n个节点

---
+++ src/main/java/com/zhang/bruce/algorithm/linked/ClimbStairSolution.java
@@ -0,0 +1,21 @@
+package com.zhang.bruce.algorithm.linked;
+
+/**
+ * @author bruce
+ * @version 1.0
+ * @date 2021/12/2 16:57
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * <p>
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ */
+public class ClimbStairSolution {
+    public int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
