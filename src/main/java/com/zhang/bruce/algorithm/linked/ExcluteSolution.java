cc50eb21de9012797801a3a24ff59db254e43921
zhangpan1 <852857754@qq.com>
2021-12-03 18:55:14 +0800

    删除排序数组中的重复项

---
+++ src/main/java/com/zhang/bruce/algorithm/linked/ExcluteSolution.java
@@ -0,0 +1,34 @@
+package com.zhang.bruce.algorithm.linked;
+
+/**
+ * @author bruce
+ * @version 1.0
+ * @date 2021/12/3 18:53
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成
+ */
+public class ExcluteSolution {
+    public int removeDuplicates1(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int t = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) nums[t++] = nums[i];
+        }
+        return t;
+    }
+}
