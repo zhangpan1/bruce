1c8c027f46f62fbb2c7e03b0ed7dbd761b1a4439
zhangpan1 <852857754@qq.com>
2021-12-15 19:23:43 +0800

    当一个无情的刷题机器

---
+++ src/main/java/com/zhang/bruce/algorithm/linked/RetrySolution.java
@@ -0,0 +1,24 @@
+package com.zhang.bruce.algorithm.linked;
+
+/**
+ * @author bruce
+ * @version 1.0
+ * @date 2021/12/15 19:17
+ */
+public class RetrySolution {
+    // 反转链表
+    public ListNode resevleList(ListNode head){
+        // 先写思路
+        // 将思路转换为代码
+        // pre  --- cur  ---- next  ,指针反转 cur指向pre ,next 指向cur ，cur 往下
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr.next != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+}
