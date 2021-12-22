8826db820c8408e2e7a672996282e92daed0777b
zhangpan1 <852857754@qq.com>
2021-12-22 19:39:47 +0800

    整数反转，不太会

---
+++ src/main/java/com/zhang/bruce/algorithm/linked/Test.java
@@ -0,0 +1,13 @@
+package com.zhang.bruce.algorithm.linked;
+
+/**
+ * @author bruce
+ * @version 1.0
+ * @date 2021/12/22 17:54
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(String.valueOf(null));
+    }
+}
--- src/main/java/com/zhang/bruce/algorithm/linked/december/ReseverLink.java
+++ src/main/java/com/zhang/bruce/algorithm/linked/december/ReseverLink.java
@@ -18,17 +18,4 @@ public class ReseverLink {
         return prev;
     }
 }
-class ListNode{
-    int val;
-    ListNode next;
-    ListNode(int val,ListNode next){
-        this.val = val;
-        this.next = next;
-    }
-    ListNode(int val) {
-        this.val = val;
-    }
-    ListNode(){
 
-    }
-}
