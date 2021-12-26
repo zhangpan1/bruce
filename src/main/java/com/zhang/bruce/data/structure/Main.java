d8dfba7569074c78e665de317491e871a7c4090d
bruce <852857754@qq.com>
2021-12-26 08:09:46 +0800

    mac第一天弄好

---
+++ src/main/java/com/zhang/bruce/data/structure/Main.java
@@ -0,0 +1,10 @@
+package com.zhang.bruce.data.structure;
+
+/**
+ * @description: some desc
+ * @author: bruce
+ * @email: zhangpan380@gmail.com
+ * @date: 2021/12/25 12:44 下午
+ */
+public class Main {
+}
