96981edee0fc31e84387a818e8a7fce5d7b66aa9
bruce <852857754@qq.com>
2021-12-31 14:27:18 +0800

    自增id问题解决

--- src/main/java/com/zhang/bruce/data/season1/NewTwoSubject.java
+++ src/main/java/com/zhang/bruce/data/season1/NewTwoSubject.java
@@ -36,4 +36,20 @@ public class NewTwoSubject {
         }
         return pre.next;
     }
+    public int[] 冒泡排序(int[] array){
+        if (array.length == 0) {
+            return array;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length -1 -i; j ++) {
+                if (array[j + 1] < array[j]) {
+                    int tem = array[j + 1] ;
+                    array[j + 1] = array[j];
+                    array[j] = tem;
+                }
+            }
+        }
+
+        return array;
+    }
 }
