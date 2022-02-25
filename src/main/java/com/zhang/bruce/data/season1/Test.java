c87ea382545209b3fc43446ee8491b50ab142b12
bruce <zhangpan380@gmail.com>
2022-02-25 11:04:33 +0800

    mac第一天弄好

---
+++ src/main/java/com/zhang/bruce/data/season1/Test.java
@@ -0,0 +1,13 @@
+package com.zhang.bruce.data.season1;
+
+/**
+ * @description: some desc
+ * @author: bruce
+ * @email: zhangpan380@gmail.com
+ * @date: 2022/2/25 11:03 上午
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("mac不好用啊");
+    }
+}
