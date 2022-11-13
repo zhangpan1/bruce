8248607aa4bb9faf1b96aac168a04cce710664b5
pan.zhang <pan.zhang@yeepay.com>
2022-11-13 22:49:32 +0800

    commit

--- src/main/java/com/zhang/bruce/java8/TestCn.java
+++ src/main/java/com/zhang/bruce/java8/TestCn.java
@@ -1,6 +1,14 @@
 package com.zhang.bruce.java8;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @description: some desc
@@ -9,7 +17,106 @@ import java.time.LocalDateTime;
  */
 public class TestCn {
     public static void main(String[] args) {
-        System.out.println(LocalDateTime.now().plusDays(-1));
-        System.out.println(LocalDateTime.now());
+        Menu menu1 = new Menu();
+        Map<String,String> menu = Maps.newHashMap();
+        menu.put("lable","GPT名单管理|国家名单");
+        menu.put("value","1");
+
+        Map<String,String> menu2 = Maps.newHashMap();
+        menu2.put("lable","交易参数管理");
+        menu2.put("value","2");
+        List<Map<String,String>> m1=Lists.newArrayList();
+        m1.add(menu);
+        m1.add(menu2);
+        menu1.setMenu(m1);
+
+
+        Map<String,Map<String,String>> m2= Maps.newHashMap();
+        m2.put("lable",menu2);
+
+
+
+
+
+
+        Map<String, List<GPTSysConfig>> result = new HashMap<>();
+        GPTSysConfig gptSysConfig = JSON.parseObject("{\n" +
+                "      \"id\": null,\n" +
+                "      \"sysType\": 155,\n" +
+                "      \"typeName\": null,\n" +
+                "      \"sysTypeName\": \"风险等级\",\n" +
+                "      \"sysCode\": null,\n" +
+                "      \"sysName\": null,\n" +
+                "      \"sysMenu\": null,\n" +
+                "      \"sysScope\": null,\n" +
+                "      \"indexNo\": null,\n" +
+                "      \"sysStatus\": null,\n" +
+                "      \"remark\": null,\n" +
+                "      \"creator\": null,\n" +
+                "      \"modifier\": null,\n" +
+                "      \"gmtCreate\": null,\n" +
+                "      \"gmtModified\": null\n" +
+                "    }",GPTSysConfig.class);
+        List<GPTSysConfig> gptSysConfigs1 = Lists.newArrayList();
+        gptSysConfigs1.add(gptSysConfig);
+        result.put("1",gptSysConfigs1);
+        GPTSysConfig gpt2 = JSON.parseObject(" {\n" +
+                "      \"id\": null,\n" +
+                "      \"sysType\": 100,\n" +
+                "      \"typeName\": null,\n" +
+                "      \"sysTypeName\": \"LN核查单状态\",\n" +
+                "      \"sysCode\": null,\n" +
+                "      \"sysName\": null,\n" +
+                "      \"sysMenu\": null,\n" +
+                "      \"sysScope\": null,\n" +
+                "      \"indexNo\": null,\n" +
+                "      \"sysStatus\": null,\n" +
+                "      \"remark\": null,\n" +
+                "      \"creator\": null,\n" +
+                "      \"modifier\": null,\n" +
+                "      \"gmtCreate\": null,\n" +
+                "      \"gmtModified\": null\n" +
+                "    }",GPTSysConfig.class);
+        GPTSysConfig gpt3 = JSON.parseObject(" {\n" +
+                "      \"id\": null,\n" +
+                "      \"sysType\": 101,\n" +
+                "      \"typeName\": null,\n" +
+                "      \"sysTypeName\": \"LN处置结果\",\n" +
+                "      \"sysCode\": null,\n" +
+                "      \"sysName\": null,\n" +
+                "      \"sysMenu\": null,\n" +
+                "      \"sysScope\": null,\n" +
+                "      \"indexNo\": null,\n" +
+                "      \"sysStatus\": null,\n" +
+                "      \"remark\": null,\n" +
+                "      \"creator\": null,\n" +
+                "      \"modifier\": null,\n" +
+                "      \"gmtCreate\": null,\n" +
+                "      \"gmtModified\": null\n" +
+                "    }",GPTSysConfig.class);
+        GPTSysConfig gpt1 = JSON.parseObject(" {\n" +
+                "      \"id\": null,\n" +
+                "      \"sysType\": 102,\n" +
+                "      \"typeName\": null,\n" +
+                "      \"sysTypeName\": \"是否上报\",\n" +
+                "      \"sysCode\": null,\n" +
+                "      \"sysName\": null,\n" +
+                "      \"sysMenu\": null,\n" +
+                "      \"sysScope\": null,\n" +
+                "      \"indexNo\": null,\n" +
+                "      \"sysStatus\": null,\n" +
+                "      \"remark\": null,\n" +
+                "      \"creator\": null,\n" +
+                "      \"modifier\": null,\n" +
+                "      \"gmtCreate\": null,\n" +
+                "      \"gmtModified\": null\n" +
+                "    }",GPTSysConfig.class);
+        List<GPTSysConfig> gptSysConfigs2 = Lists.newArrayList();
+        gptSysConfigs2.add(gpt1);
+        gptSysConfigs2.add(gpt2);
+        gptSysConfigs2.add(gpt3);
+        result.put("2",gptSysConfigs2);
+        menu1.setCategory(result);
+        System.out.println(JSON.toJSONString(menu1));
     }
 }
--- src/main/java/com/zhang/bruce/java8/TestNa.java
+++ src/main/java/com/zhang/bruce/java8/TestNa.java
@@ -1,8 +1,15 @@
 package com.zhang.bruce.java8;
 
+import com.aerospike.client.admin.User;
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.zhang.bruce.java8.mituan.UnionSearch;
 
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -12,20 +19,23 @@ import java.util.Random;
  */
 public class TestNa {
     public static void main(String[] args) {
-        List<String> products = Lists.newArrayList();
-        products.add("FE");
-        products.add("RECHARGE");
-        products.add("NOCARDPAY");
-        products.add("ACCOUNT");
-        products.add("G3MEMBER_PAYMENT");
-        products.add("G3MEMBER_TRANSFER");
-        products.add("TRANSFER");
+        GPTSysConfig gptSysConfig = JSON.parseObject("{\n" +
+                "      \"id\": null,\n" +
+                "      \"sysType\": 155,\n" +
+                "      \"typeName\": null,\n" +
+                "      \"sysTypeName\": \"风险等级\",\n" +
+                "      \"sysCode\": null,\n" +
+                "      \"sysName\": null,\n" +
+                "      \"sysMenu\": null,\n" +
+                "      \"sysScope\": null,\n" +
+                "      \"indexNo\": null,\n" +
+                "      \"sysStatus\": null,\n" +
+                "      \"remark\": null,\n" +
+                "      \"creator\": null,\n" +
+                "      \"modifier\": null,\n" +
+                "      \"gmtCreate\": null,\n" +
+                "      \"gmtModified\": null\n" +
+                "    }",GPTSysConfig.class);
 
-        for (int i = 0 ;i < 20 ;i ++) {
-            Random r = new Random();
-            int data = r.nextInt(7);
-            System.out.println(products.get(data));
-            System.out.println(data);
-        }
     }
 }
